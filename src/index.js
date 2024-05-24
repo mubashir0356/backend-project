2522c39acd17b305be21ce18418e404a3641c40b
Abdul Mubashir <mubashir0356@gmail.com>
2024-05-24 15:12:33 +0530

    app initialized

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import dotenv from "dotenv"
 import connectDatabase from './database/database.js'
+import { app } from "./app.js"
+
 
 dotenv.config({
     path: './.env'
 })
 
-connectDatabase()
+const port = process.env.PORT || 5000
+
+connectDatabase().then(() => {
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`)
+    })
+}).catch(error => console.error("Error while connecting DB ", error))
