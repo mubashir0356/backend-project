ba1295d9ac45dc24381bbd467c46757fc8f59d23
Abdul Mubashir <mubashir0356@gmail.com>
2024-05-25 19:57:12 +0530

    file uploading- multer and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/temp') // specify the destination folder here
+    },
+    filename: function (req, file, cb) {
+        console.log("multer file", file)
+        cb(null, file.originalname) // specify the name of file -- should be unique else it will overwrite existing file
+    }
+})
+
+export const upload = multer({ storage: storage })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// uploading file to cloudinary server from local server
+
+const uploadToCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("File uploaded successfully as:", response.url);
+        fs.unlinkSync(localFilePath) // removing the locally saved file as it is saved to cloudinary server
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // removing the locally saved file since its failed to be uploaded
+    }
+}
+
+export { uploadToCloudinary }
